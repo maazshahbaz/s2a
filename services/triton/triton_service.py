54f442d15485028de13a905351f944ec559058c8
maazshahbaz <maazshahbaz123@gmail.com>
2026-01-14 21:44:29 +0500

    Fixed issue

--- services/triton/triton_service.py
+++ services/triton/triton_service.py
@@ -5,6 +5,6 @@ from intelligent_pipeline.pipeline import Pipeline
 async def run_async_pipeline(audio_path: str, request_id: str, callback = None):
     """Convenience function to run the pipeline synchronously (e.g. for scripts)"""
     pipeline = Pipeline()
-    raw_trans, labeled_trans, analysis, diar_info = await pipeline.run_pipeline(audio_path, request_id)
+    raw_transcription, labeled_transcription, analysis, metadata = await pipeline.run_pipeline(audio_path, request_id)
     if callback:
-        callback(raw_trans, labeled_trans, analysis, diar_info)
+        callback(raw_transcription, labeled_transcription, analysis, metadata)
